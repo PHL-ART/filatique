e6b50f3c2100d0f941ab4ed4b59296750a525ebd
Filat Astakhov <astakhovfilat@gmail.com>
2025-12-02 14:34:56 +0300

    Few design fixes

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -1,5 +1,33 @@
 import { Header } from "@shared/ui/Header";
+import { Anchor, Box } from "@mantine/core";
+import classes from "./page.module.css";
 
 export default function Info() {
-  return <Header>Info</Header>;
+  const linkFont = "'JostExtraBold', sans-serif";
+
+  return (
+    <>
+      <Header>
+        <Box className={classes.navigationLinks}>
+          <Anchor
+            href="#bio"
+            className={classes.navigationLink}
+            style={{ fontFamily: linkFont }}
+          >
+            Bio
+          </Anchor>
+          <span className={classes.navigationDivider} aria-hidden="true">
+            &middot;
+          </span>
+          <Anchor
+            href="#contacts"
+            className={classes.navigationLink}
+            style={{ fontFamily: linkFont }}
+          >
+            Contacts
+          </Anchor>
+        </Box>
+      </Header>
+    </>
+  );
 }
--- src/app/listen/page.tsx
+++ src/app/listen/page.tsx
@@ -4,17 +4,27 @@ import { Anchor, Box } from "@mantine/core";
 import classes from "./page.module.css";
 
 export default function Listen() {
+  const linkFont = "'JostExtraBold', sans-serif";
+
   return (
     <>
       <Header>
         <Box className={classes.navigationLinks}>
-          <Anchor href="#releases" className={classes.navigationLink}>
+          <Anchor
+            href="#releases"
+            className={classes.navigationLink}
+            style={{ fontFamily: linkFont }}
+          >
             Releases
           </Anchor>
           <span className={classes.navigationDivider} aria-hidden="true">
             &middot;
           </span>
-          <Anchor href="#mixes" className={classes.navigationLink}>
+          <Anchor
+            href="#mixes"
+            className={classes.navigationLink}
+            style={{ fontFamily: linkFont }}
+          >
             Mixes
           </Anchor>
         </Box>
